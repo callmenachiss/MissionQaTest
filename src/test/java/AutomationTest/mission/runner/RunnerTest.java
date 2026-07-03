0d3f96d3b9101c2f83f5de81de09bac778ad3460
Nachiyappan Jeya <nachiyappan7777@gmail.com>
2026-07-03 13:38:15 +0530

    added all files

--- src/test/java/AutomationTest/mission/runner/RunnerTest.java
+++ src/test/java/AutomationTest/mission/runner/RunnerTest.java
@@ -1,12 +1,8 @@
 package AutomationTest.mission.runner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.CucumberFeatureWrapper;
-import cucumber.api.testng.TestNGCucumberRunner;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = {
@@ -17,36 +13,19 @@ import org.testng.annotations.Test;
                 "AutomationTest.mission.hooks",
                 "AutomationTest.mission.steps"
         },
-        strict = true,
         monochrome = true,
-        format = {
+        plugin = {
                 "pretty",
-                "html:test-output/cucumber-reports/cucumber-pretty",
+                "html:test-output/cucumber-reports/report.html",
                 "json:test-output/cucumber-reports/CucumberTestReport.json",
-                "rerun:test-output/cucumber-reports/rerun.txt",
-                "testng:test-output/cucumber-reports/testng.xml"
-        })
-public class RunnerTest {
+                "rerun:test-output/cucumber-reports/rerun.txt"
+        }
+)
+public class RunnerTest extends AbstractTestNGCucumberTests {
 
-    private TestNGCucumberRunner testNGCucumberRunner;
-
-    @BeforeClass(alwaysRun = true)
-    public void setUpClass() {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-    }
-
-    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
-    public void feature(CucumberFeatureWrapper cucumberFeature) {
-        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-    }
-
-    @DataProvider
-    public Object[][] features() {
-        return testNGCucumberRunner.provideFeatures();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() {
-        testNGCucumberRunner.finish();
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
     }
 }
